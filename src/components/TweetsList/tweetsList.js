16ad2a8c0d8ad2f9917432f434794ebaa0e14f81
CarlosRosas25 <cjrosasbrito@gmail.com>
2022-04-02 13:40:22 -0500

    Add loading animation

--- src/components/TweetsList/tweetsList.js
+++ src/components/TweetsList/tweetsList.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { TweetsContext } from "../../contexts/TweetsContext";
 import useTweetCollection from "../../hooks/useTweetCollection";
 import Tweet from "../Tweet/tweet";
+import "./styles.css";
 
 function TweetsList() {
   const { tweetsArray } = useContext(TweetsContext);
@@ -11,16 +12,21 @@ function TweetsList() {
     const getTweetsAsync = async () => {
       const allTweets = await getTweets();
     };
-    getTweetsAsync();
+
+    setTimeout(() => {
+      getTweetsAsync();
+    }, 5000);
   }, []);
 
   return (
     <div className="tweetsList">
-      {tweetsArray.length > 0
-        ? tweetsArray.map((object) => {
-            return <Tweet key={object.id} tweetData={object} />;
-          })
-        : "0 tweets"}
+      {tweetsArray.length > 0 ? (
+        tweetsArray.map((object) => {
+          return <Tweet key={object.id} tweetData={object} />;
+        })
+      ) : (
+        <div className="loading"></div>
+      )}
     </div>
   );
 }
